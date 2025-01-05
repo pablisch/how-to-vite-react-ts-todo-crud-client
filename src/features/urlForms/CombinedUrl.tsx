4c0c8e0e6a8a85db6dee8c56278d286383a19ef4
Pablo <pablito@me.com>
2025-01-05 23:04:26 +0000

    chore: working mock complete url.

---
+++ src/features/urlForms/CombinedUrl.tsx
@@ -0,0 +1,56 @@
+import { useBaseUrl } from '../../hooks/useBaseUrl.tsx'
+import { useEndpoint } from '../../hooks/useEndpoint.tsx'
+import { useIdParams } from '../../hooks/useIdParams.tsx'
+import { useQueryParams } from '../../hooks/useQueryParams.tsx'
+import React, { useEffect, useState } from 'react'
+import { useSettings } from '../../hooks/useSettings.tsx'
+
+const CombinedUrl = () => {
+  const { baseUrl } = useBaseUrl()
+  const { endpoint } = useEndpoint()
+  const { idParams } = useIdParams()
+  const { queryParams } = useQueryParams()
+  const { settings } = useSettings()
+  const combinedValue = `${baseUrl}${endpoint}${idParams}${queryParams}`
+  const [inputValue, setInputValue] = useState<string>(combinedValue)
+
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // setInputValue(event.target.value)
+    setInputValue(inputValue) // TODO nonsense placeholder line
+    console.log('****()** event:', event.target.value)
+    console.log('****()** settings:', JSON.stringify(settings, null, 2))
+  }
+
+  useEffect(() => {
+    setInputValue(combinedValue)
+  }, [combinedValue])
+
+  return (
+    <div className="url-form" id="combined-url-form">
+      <h1 id="complete-url-title" className={`url-form-title`}>
+        Complete URL:
+      </h1>
+      {/* styled overlay */}
+      <div className={`url-overlay-container combined-input`}>
+        <div
+          className={`url-overlay`}
+        >
+          <span className="base-url-text mono space-left-18">{baseUrl}</span>
+          <span className="url-endpoint-text mono">{endpoint}</span>
+          <span className="url-id-param-text mono">{idParams}</span>
+          <span className="url-query-param-text mono">{queryParams}</span>
+        </div>
+        {/* hidden input element */}
+        <input
+          type="text"
+          className="url-input mono combined-input hidden-input-element"
+          value={combinedValue}
+          onChange={handleInputChange}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default CombinedUrl
--- src/features/urlForms/UrlComponents.tsx
+++ src/features/urlForms/UrlComponents.tsx
@@ -8,21 +8,30 @@ import { useBaseUrl } from '../../hooks/useBaseUrl.tsx'
 import { useEndpoint } from '../../hooks/useEndpoint.tsx'
 import { useIdParams } from '../../hooks/useIdParams.tsx'
 import { useQueryParams } from '../../hooks/useQueryParams.tsx'
+import CombinedUrl from './CombinedUrl.tsx'
+import { useEffect, useState } from 'react'
 
 const UrlComponents = () => {
   const { baseUrl } = useBaseUrl()
   const { endpoint } = useEndpoint()
   const { idParams } = useIdParams()
   const { queryParams } = useQueryParams()
-  const combinedUrl = `${baseUrl}${endpoint}${idParams}${queryParams}`
-  console.log('combinedUrl:', combinedUrl)
+  const [url, setUrl] = useState<string>(
+    `${baseUrl}${endpoint}${idParams}${queryParams}`
+  )
+  console.log('combinedUrl:', url)
+
+  useEffect(() => {
+    setUrl(`${baseUrl}${endpoint}${idParams}${queryParams}`)
+  }, [baseUrl, endpoint, idParams, queryParams])
 
   return (
     <div
       className="flex-container url-components-container"
       id="url-components-container"
     >
-      <CompleteUrl combinedUrl={combinedUrl} />
+      <CompleteUrl combinedUrl={url} />
+      <CombinedUrl combinedUrl={url} />
       <BaseUrlForm />
       <EndpointUrlForm />
       <IdParamsForm />
