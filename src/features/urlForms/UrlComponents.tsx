e959b6db72489b98dd6d501f738e21b8dffe90e7
Pablo <pablito@me.com>
2025-01-05 18:13:27 +0000

    chore: set complete url as an input.

--- src/features/urlForms/UrlComponents.tsx
+++ src/features/urlForms/UrlComponents.tsx
@@ -4,14 +4,25 @@ import EndpointUrlForm from './EndpointUrlForm.tsx'
 import IdParamsForm from './IdParamsForm.tsx'
 import QueryParamsForm from './QueryParamsForm.tsx'
 import CompleteUrl from './CompleteUrl.tsx'
+import { useBaseUrl } from '../../hooks/useBaseUrl.tsx'
+import { useEndpoint } from '../../hooks/useEndpoint.tsx'
+import { useIdParams } from '../../hooks/useIdParams.tsx'
+import { useQueryParams } from '../../hooks/useQueryParams.tsx'
 
 const UrlComponents = () => {
+  const { baseUrl } = useBaseUrl()
+  const { endpoint } = useEndpoint()
+  const { idParams } = useIdParams()
+  const { queryParams } = useQueryParams()
+  const combinedUrl = `${baseUrl}${endpoint}${idParams}${queryParams}`
+  console.log('combinedUrl:', combinedUrl)
+
   return (
     <div
       className="flex-container url-components-container"
       id="url-components-container"
     >
-      <CompleteUrl />
+      <CompleteUrl combinedUrl={combinedUrl} />
       <BaseUrlForm />
       <EndpointUrlForm />
       <IdParamsForm />
