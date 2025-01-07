44bcea90b20cef0e3f66502465b17d16d6c011b7
Pablo <pablito@me.com>
2025-01-07 20:24:11 +0000

    chore: restored proper functionality to operation indicators.

--- src/features/singleItemPane/OperationIndicator.tsx
+++ src/features/singleItemPane/OperationIndicator.tsx
@@ -7,27 +7,27 @@ const OperationIndicator = () => {
   return (
     <div className="operation-indicator-container">
       <div
-        className={`operation ${operation === 'getById' ? 'active-operation active-get-by-id' : 'active-operation active-get-by-id'}`}
+        className={`operation ${operation === 'getById' ? 'active-operation active-get-by-id' : ''}`}
       >
         GET items/:id
       </div>
       <div
-        className={`operation ${operation === 'create' ? 'active-operation active-post' : 'active-operation active-post'}`}
+        className={`operation ${operation === 'create' ? 'active-operation active-post' : ''}`}
       >
         POST items
       </div>
       <div
-        className={`operation ${operation === 'delete' ? 'active-operation active-delete' : 'active-operation active-delete'}`}
+        className={`operation ${operation === 'delete' ? 'active-operation active-delete' : ''}`}
       >
         DELETE items
       </div>
       <div
-        className={`operation ${operation === 'update' && isPatchUpdate ? 'active-operation active-patch' : ' active-operation active-patch'}`}
+        className={`operation ${operation === 'update' && isPatchUpdate ? 'active-operation active-patch' : ''}`}
       >
         PATCH items/:id
       </div>
       <div
-        className={`operation ${operation === 'update' && !isPatchUpdate ? 'active-operation active-put' : 'active-operation active-put'}`}
+        className={`operation ${operation === 'update' && !isPatchUpdate ? 'active-operation active-put' : ''}`}
       >
         PUT items/:id
       </div>
