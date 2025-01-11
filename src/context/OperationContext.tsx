e7c548a30c2ea8f4c89eb87d1e03c11186279834
Pablo <pablito@me.com>
2025-01-11 16:58:51 +0000

    chore: Added handling operation change functions to OperationContext.

--- src/context/OperationContext.tsx
+++ src/context/OperationContext.tsx
@@ -4,21 +4,35 @@ const initialOperation = localStorage.getItem('operation') || 'getById'
 
 export interface OperationContextType {
   operation: string
+  handleChangeOperation: (newOperation: string) => void
+  handleResetOperation: () => void
   setOperation: (operation: string) => void
 }
 
 export const OperationContext = createContext<OperationContextType>({
   operation: initialOperation,
+  handleChangeOperation: () => {},
+  handleResetOperation: () => {},
   setOperation: () => {},
 })
 
 export const OperationProvider = ({ children }: { children: ReactNode }) => {
   const [operation, setOperation] = useState<string>(initialOperation)
 
+  const handleChangeOperation = (newOperation: string) => {
+    if (newOperation !== operation) setOperation(newOperation)
+  }
+
+  const handleResetOperation = () => {
+    if (operation !== 'getById') setOperation('getById')
+  }
+
   return (
     <OperationContext.Provider
       value={{
         operation,
+        handleChangeOperation,
+        handleResetOperation,
         setOperation,
       }}
     >
