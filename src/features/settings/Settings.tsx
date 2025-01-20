8c31270c6d8e4687bf6f571cd03951a3bcd14e1b
Pablo <pablito@me.com>
2025-01-20 19:47:00 +0000

    chore: set information in settings container and formatted to allow for coherent implementation of url saved items reset..

--- src/features/settings/Settings.tsx
+++ src/features/settings/Settings.tsx
@@ -1,5 +1,6 @@
 import Button from '../../components/Button.tsx'
 import { useSettings } from '../../hooks/useSettings.tsx'
+import './Settings.css'
 
 const Settings = () => {
   const { settings, handleSetUrlSetMode } = useSettings()
@@ -9,14 +10,23 @@ const Settings = () => {
       className="whole-width-container flex-container border-primary"
       id="settings-container"
     >
-      <Button
-        ariaLabel="set url section setting behaviour"
-        id={`set-url-mode`}
-        className={`btn url-btn`}
-        onClick={handleSetUrlSetMode}
-      >
-        {`Set URL section ${settings.setUrlOnChange ? 'on submit' : 'as you type'}`}
-      </Button>
+      <div className="set-url-options-container settings-section flex-container">
+        <div className="settings-left-wrapper">
+        
+        <h1 className="set-url-options-header settings-header">Choose if URL sections are set as you type or only on submission:</h1>
+        </div>
+        <div className="settings-right-wrapper">
+        
+        <Button
+          ariaLabel="set url section setting behaviour"
+          id={`set-url-mode`}
+          className={`btn url-btn`}
+          onClick={handleSetUrlSetMode}
+        >
+          {`Set URL section ${settings.setUrlOnChange ? 'on submit' : 'as you type'}`}
+        </Button>
+        </div>
+      </div>
     </div>
   )
 }
